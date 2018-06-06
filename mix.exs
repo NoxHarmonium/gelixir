e23641fcdb1a8a6f1defe9d35c92dbcd2c529372
Sean Dawson <contact@seandawson.info>
2018-06-07 08:02:43 +1000

    Fix naming

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Gelixer.MixProject do
 
   def project do
     [
-      app: :gelixer,
+      app: :gelixir,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
--- test/gelixer_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GelixerTest do
-  use ExUnit.Case
-  doctest Gelixer
-
-  test "greets the world" do
-    assert Gelixer.hello() == :world
-  end
-end
